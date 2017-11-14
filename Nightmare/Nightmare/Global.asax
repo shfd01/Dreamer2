b483531c10822d50d109530f7d94790c41acd7eb
joonghee ahn <steelleg@gmail.com>
2017-11-14 15:34:07 +0900

    add nightmare solution for tutorial

---
+++ Nightmare/Nightmare/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nightmare.MvcApplication" Language="C#" %>
---
+++ Nightmare/Nightmare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nightmare.Startup))]
+namespace Nightmare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
